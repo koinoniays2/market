9f20fbfd0e3502eb4e9715e870b2ef22d902570e
Jin young <xmfkdnak98@gmail.com>
2024-04-23 06:45:26 +0900

    feat: slide 추가

--- src/app/(landing)/page.jsx
+++ src/app/(landing)/page.jsx
@@ -14,7 +14,7 @@ const slideContents = [
     sub: "플리마켓 기본 셋팅 물품 제공",
     contents:
       "테이블, 스탠드, 마네킹 등 제공해드립니다. 오직 고객과 상품판매에만 집중해주세요!",
-    image: "MarketVendor.svg",
+    image: "/svg/MarketVendor.svg",
   },
 
   {
@@ -22,7 +22,7 @@ const slideContents = [
     sub: "빠르고 간편한 플리마켓 참가신청",
     contents:
       "안전한 마켓 참가를 위해 불필요한 개인정보는요구하지 않습니다.단, 판매제품의 안전과 인증을 위해별도의 정보요청할 수 있습니다.",
-    image: "QueicklyAndEasily.svg",
+    image: "/svg/QuicklyAndEasily.svg",
   },
 
   {
@@ -30,15 +30,14 @@ const slideContents = [
     sub: "스트릿마켓 친구추가 고객에게마켓 알림톡 전송",
     contents:
       "스트릿마켓을 친구추가한 분들께플리마켓 알림톡 전송을 합니다.알림톡 서비스로 홍보효과를 누려보세요!.",
-    image: "PromotionalAlert.svg",
+    image: "/svg/PromotionalAlert.svg",
   },
 ];
 
-const map = [];
 
-for (let i = 0; i < slideContents.length; i++) {
-  map.push(<Slide obj={slideContents[i]} />);
-}
+
+
+
 
 export default function LandingPage() {
   const [slideIndex, setSlideIndex] = useState(0);
@@ -50,7 +49,7 @@ export default function LandingPage() {
         <SlideContainer slideIndex={slideIndex} setSlideIndex={setSlideIndex}>
           <Hero />
           <About />
-          <div>{map}</div>
+          {slideContents.map((element, index)=>(<Slide key={index} obj={element} />))}
           <Footer />
         </SlideContainer>
       </main>
--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,37 +1,39 @@
 import { useEffect, useRef } from "react";
 
 export default function Slide({ obj }) {
-  const objRef = useRef(null);
 
-  useEffect(
-    () => {
-      const observer = new IntersectionObserver((entry) => {
-        if (entry[0].isIntersecting) {
-          console.log("gd");
-        }
-      });
-      observer.observe(objRef.current);
-    },
-    {
-      threshold: 0.5,
-    }
-  );
+  const imageContainer = useRef(null);
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver((entry) => {
+      const image = entry[0]
+      if(image.isIntersecting){
+        setTimeout(()=>{
+          image.target.style.opacity = "1"
+          image.target.style.right = "50%"
+          image.target.style.transform = "translateX(50%)"
+        },1000)
+      }
+    })
+
+    if(imageContainer.current) observer.observe(imageContainer.current);
+    
+  },[])
 
   return (
-    <div className="h-lvh flex flex-col justify-around px-[20px] bg-[#FB9E01]">
-      {/* 컨테이너 */}
-      <div className="flex flex-col pt-10 text-black">
-        {/* 타이틀 */}
-        <div className="w-full text-4xl mb-5 font-semibold">{obj.title}</div>
-        {/* 서브타이틀 */}
-        <div className="mb-2.5 text-[18px] font-medium">{obj.sub}</div>
-        {/* 콘텐츠 */}
-        <div className="text-[14px] mb-[38px]">{obj.contents}</div>
+    <section className="relative">
+      <h2 className="text-[36px]">{obj.title}</h2>
+      <h3 className="text-[18px] pt-[10px]">{obj.sub}</h3>
+      <div className="text-[16px] pt-[20px]">
+        <p>{obj.contents}</p>
       </div>
-      {/* 이미지 */}
-      <div ref={objRef} className="w-full flex justify-center">
-        <img src={obj.image} alt="" />
+      <div ref={imageContainer} className={`absolute pt-[100px] duration-1000 opacity-0 -right-1/3 ${obj.image === "/svg/QuicklyAndEasily.svg" ? "w-[250px]" : "w-[300px]"}`}>
+      <img 
+          src={obj.image} 
+          alt="About Image"
+          className="w-full" 
+        />
       </div>
-    </div>
+    </section>
   );
 }
