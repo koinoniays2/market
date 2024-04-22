a1c7c1272fff85d9cf24ded45e57e61faec1d368
Jinyoung Choi <xmfkdnak98@gmail.com>
2024-04-22 16:37:46 +0900

    feat: 모바일 환경에서 터치로도 슬라이드 넘길 수 있게 기능 추가

--- src/components/SlideContainer/SlideContainer.jsx
+++ src/components/SlideContainer/SlideContainer.jsx
@@ -7,6 +7,7 @@ import throttel from "@/utils/throttel";
 export default function SlideContainer( {children, slideIndex, setSlideIndex} ) {
 
   const [slideLength, setSlideLength] = useState(0);
+  const [touchStartClientY, setTouchStartClientY] = useState(null);
   const delay = 1000;
   const container = useRef(null);
   const inThrottle = useRef(false);
@@ -25,11 +26,28 @@ export default function SlideContainer( {children, slideIndex, setSlideIndex} ) 
     }
   }
   const handleWheel = throttel(onScrollChange, delay, inThrottle);
-   
+  
+  const handleTouchStart = (event) => {
+    setTouchStartClientY(event.touches[0].clientY)
+  }
+
+  const onTouchChange = (event) => {
+    const touchMoveClientY = event.touches[0].clientY;
+
+    if(touchStartClientY > touchMoveClientY) {
+      setSlideIndex(slideIndex + 1);
+    } else {
+      setSlideIndex(slideIndex - 1);
+    }
+  }
+  const handleTouchMove = throttel(onTouchChange, delay, inThrottle)
+
   return (
     <div 
       ref={container} 
-      onWheel={handleWheel} 
+      onWheel={handleWheel}
+      onTouchStart={handleTouchStart}
+      onTouchMove={(handleTouchMove)}
       className={`w-full duration-1000 `}
       style={{transform : `translateY(-${slideIndex * 100}vh)`}}
     >
