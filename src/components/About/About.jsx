fe42a27f2c25dd586a6377bcdcd5305a13dc45b0
Jin young <xmfkdnak98@gmail.com>
2024-04-23 06:44:15 +0900

    feat: about 슬라이드 추가

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,28 @@
+
 export default function About() {
   return (
-    <div>About</div>
+    <section>
+      <h2 className="text-[46px]">About</h2>
+      <div className="text-[14px]">
+        <p>COVID-19 종식 후,</p>
+        <p>빠르게 성장하는 플리마켓 문화는 독특한 상품과</p>
+        <p>예술 작품, 그리고 지역 특색이 담긴 제품 등을</p>
+        <p>특징으로 하며, 환경과 경제적 가치를 중시하는 2030세대의 새로운 트렌드 중 하나입니다.</p>
+      </div>
+      <br />
+      <div className="text-[14px]" >
+        <p>우리는 올바른 플리마켓 문화를 세우기 위해 지속적으로</p>
+        <p>플리마켓을 개최하고 다양한 셀러를 모집하여</p>
+        <p>셀러들과 플리마켓 이용객이 모두 만족하는</p>
+        <p>서비스를 제공하기 위해 노력하는 회사입니다.</p>
+      </div>
+      <div className="pt-[50px]">
+        <img 
+          src="/svg/About.svg" 
+          alt="About Image"
+          className="w-full" 
+        />
+      </div>
+    </section>
   )
 }
