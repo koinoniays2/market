2551483b2f95484d445f633e0abb945b51a9766e
Jin young <xmfkdnak98@gmail.com>
2024-04-24 10:32:36 +0900

    design: 반응형 추가, 배경사진 추가

--- src/components/MsgSection/MsgSection.jsx
+++ src/components/MsgSection/MsgSection.jsx
@@ -17,7 +17,7 @@ export default function MsgSection() {
           }, 500 * index)
         });
       }
-    })
+    }, {threshold: 1})
 
     if(myMsg.current) observer.observe(myMsg.current);
 
@@ -32,43 +32,52 @@ export default function MsgSection() {
         <p>스트릿마켓에는 자체 CS팀이 준비되어있습니다.</p>
         <p>마켓기간 운영 중,  발생된 문제는 스트릿마켓 CS팀에게 맡겨주세요!</p>
       </div>
-      <div ref={myMsg} className="w-full space-y-1 text-[14px] pt-5">
-        <div data-aos="zoom-in-right" className="w-full">
-          <div className="relative bg-white ml-[20px] p-3 w-[240px] rounded-2xl border-[3px] border-black">
-            <p>물건을 구매했는데 어느 셀러에게서 구매했는지 모르겠어요!</p>
-            <img 
-              className="w-5 absolute -left-[19px] top-3"
-              src="/svg/bubble_tail_white.svg" 
-              alt="bubble_tail" />
-          </div>            
-        </div>
-        <div data-aos="zoom-in-left" className="w-full flex flex-row-reverse">
-          <div className="relative bg-[#FFEF5C] mr-[20px] p-3 w-[240px] rounded-2xl border-[3px] border-black">
-            <p>안녕하세요 고객냄!</p>
-            <p>영수증 또는 상품사진 첨부해주시면 확인 후 연결시켜드리겠습니다.</p>
-            <img 
-              className="w-5 absolute -right-[19px] top-3 scaleX scale-x-[-1] "
-              src="/svg/bubble_tail_gold.svg" 
-              alt="bubble_tail" />
-          </div>            
-        </div>
-        <div data-aos="zoom-in-right" className="w-full">
-          <div className="relative bg-white ml-[20px] p-3 w-[260px] rounded-2xl border-[3px] border-black">
-            <p>물건을 구매했는데 집에서 확인하니 상품이 파손되어 있어요! 어떻게 해야하죠?</p>
-            <img 
-              className="w-5 absolute -left-[19px] top-3"
-              src="/svg/bubble_tail_white.svg" 
-              alt="bubble_tail" />
-          </div>   
-        </div>
-        <div data-aos="zoom-in-left" className="w-full flex flex-row-reverse">
-          <div className="relative bg-[#FFEF5C] mr-[20px] p-3 w-[240px] rounded-2xl border-[3px] border-black">
-            <p>고객님, 많이 속상하시죠? 죄송합니다. 확인 후 빠르게 도와드리겠습니다.</p>
-            <img 
-              className="w-5 absolute -right-[19px] top-3 scaleX scale-x-[-1] "
-              src="/svg/bubble_tail_gold.svg" 
-              alt="bubble_tail" />
-          </div>            
+      <div className="w-full flex flex-row-reverse justify-center lg:justify-start">
+        <div className="w-full relative max-w-[668px] text-[14px] md:text-[18px] mt-5 md:mt-[80px]">
+          <img 
+            className="hidden md:block absolute w-[300px] left-1/2 -translate-x-1/2"  
+            src="/svg/Phone.svg" 
+            alt="phone background" />
+          <div ref={myMsg} className="md:pt-10 space-y-1">
+            <div data-aos="zoom-in-right" className="w-full">
+              <div className="relative bg-white ml-[20px] p-3 md:p-5 w-[240px] md:w-[270px] rounded-2xl border-[3px] border-black">
+                <p>물건을 구매했는데 어느 셀러에게서 구매했는지 모르겠어요!</p>
+                <img 
+                  className="w-5 absolute -left-[19px] top-3"
+                  src="/svg/bubble_tail_white.svg" 
+                  alt="bubble_tail" />
+              </div>            
+            </div>
+            <div data-aos="zoom-in-left" className="w-full flex flex-row-reverse">
+              <div className="relative bg-[#FFEF5C] mr-[20px] p-3 md:p-5 w-[240px] md:w-[300px] rounded-2xl border-[3px] border-black">
+                <p>안녕하세요 고객님!</p>
+                <p>영수증 또는 상품사진 첨부해주시면 확인 후 연결시켜드리겠습니다.</p>
+                <img 
+                  className="w-5 absolute -right-[19px] top-3 scaleX scale-x-[-1] "
+                  src="/svg/bubble_tail_gold.svg" 
+                  alt="bubble_tail" />
+              </div>            
+            </div>
+            <div data-aos="zoom-in-right" className="w-full">
+              <div className="relative bg-white ml-[20px] p-3 md:p-5 w-[240px] md:w-[300px] rounded-2xl border-[3px] border-black">
+                <p>물건을 구매했는데 집에서 확인하니 상품이 파손되어 있어요!</p>
+                <p>어떻게 해야하죠?</p>
+                <img 
+                  className="w-5 absolute -left-[19px] top-3"
+                  src="/svg/bubble_tail_white.svg" 
+                  alt="bubble_tail" />
+              </div>   
+            </div>
+            <div data-aos="zoom-in-left" className="w-full flex flex-row-reverse">
+              <div className="relative bg-[#FFEF5C] mr-[20px] p-3 md:p-5 w-[240px] md:w-[310px] rounded-2xl border-[3px] border-black">
+                <p>고객님, 많이 속상하시죠? 죄송합니다. 확인 후 빠르게 도와드리겠습니다.</p>
+                <img 
+                  className="w-5 absolute -right-[19px] top-3 scaleX scale-x-[-1] "
+                  src="/svg/bubble_tail_gold.svg" 
+                  alt="bubble_tail" />
+              </div>            
+            </div>
+          </div>
         </div>
       </div>
     </section>
