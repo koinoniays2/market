aebd4f347823caff64e7a3d3334c38c7e0aecb16
Jin young <xmfkdnak98@gmail.com>
2024-04-24 10:08:37 +0900

    design: 이미지 사이즈 조정

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -3,17 +3,14 @@ import { useEffect, useRef, useState } from "react";
 export default function Slide({ obj }) {
 
   const imageContainer = useRef(null);
-  const [showImage, setShowImage] = useState("");
+  const [showImage, setShowImage] = useState("opacity-0 -right-1/3");
 
   useEffect(()=>{
     const observer = new IntersectionObserver((entry) => {
       const image = entry[0]
       if(image.isIntersecting){
         setTimeout(()=>{
-          image.target.style.opacity = "1"
-          image.target.style.right = "50%"
-          setShowImage("translate-x-[50%] md:translate-x-[70%]")
-          // image.target.style.transform = "translateX(70%)"
+          setShowImage("translate-x-[50%] md:translate-x-[70%] opacity-100 right-1/2")
         },500)
       }
     })
@@ -29,10 +26,11 @@ export default function Slide({ obj }) {
       <div className="text-[18px] md:text-[20px] pt-[20px]">
         {obj.contents.map((element, index) => (<p key={index}>{element}</p>))}
       </div>
-      <div ref={imageContainer} className={`absolute pt-[100px] duration-1000 bottom-[50px] md:bottom-[100px] opacity-0 -right-1/3 ${showImage} ${obj.image === "/svg/QuicklyAndEasily.svg" ? "w-[250px] md:w-[500px] lg:w-[550px]" : "w-[300px] md:w-[500px] lg:w-[550px]"}`}>
+      {/* <div ref={imageContainer} className={`w-full absolute pt-[100px] duration-1000 bottom-[50px] md:bottom-[100px]  ${showImage} ${obj.title === "Quickly and Easily" ? "w-[250px] md:w-[500px] lg:w-[550px]" : "w-[300px] md:w-[500px] lg:w-[550px]"}`}> */}
+      <div ref={imageContainer} className={`w-full absolute pt-[100px] duration-1000 bottom-[50px] md:bottom-[100px]  ${showImage} ${obj.title === "Quickly and Easily" ? 'w-[230px]' : 'w-[250px]' } md:w-[500px] lg:w-[550px]`}>
       <img 
           src={obj.image} 
-          alt="About Image"
+          alt={`${obj.title} Image`}
           className="w-full" 
         />
       </div>
